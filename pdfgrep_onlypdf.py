29e9945e93013447ed796051a114fb32f6ac6b75
LadyJ <91752544+Ibishiya@users.noreply.github.com>
2024-07-24 02:49:55 +0000

    nineteenth

--- pdfgrep_onlypdf.py
+++ pdfgrep_onlypdf.py
@@ -17,8 +17,6 @@ st.title('PDFgrep in Streamlit')
 
 uploaded_files = st.file_uploader("Choose PDF files", accept_multiple_files=True)
 
-results = []  # Initialize results outside the conditional block
-
 if uploaded_files:
     # Save uploaded files to a writable directory
     save_path = '/tmp/uploaded_files'
@@ -35,6 +33,7 @@ if uploaded_files:
 
     if search_term:
         st.text("Searching files...")
+        results = []
         for file_path in file_paths:
             st.write(f"Searching for '{search_term}' in {file_path}")
             
@@ -54,12 +53,12 @@ if uploaded_files:
     if st.button('Save Results'):
         st.text("Generating results files...")
         # Save as text file
-        all_results_txt = "\n".join([f"Results for {result['file_path']}:\n{result['output']}'" for result in results])
+        all_results_txt = "\n".join([f"Results for {result['file_path']}:\n{result['output']}" for result in results])
         txt_file_path = '/tmp/results.txt'
         with open(txt_file_path, 'w') as f:
             f.write(all_results_txt)
         
-        # Save as csv file
+        # Save as CSV file
         df = pd.DataFrame(results)
         csv_file_path = '/tmp/results.csv'
         df.to_csv(csv_file_path, index=False)
@@ -72,8 +71,8 @@ if uploaded_files:
         )
         
         st.download_button(
-            label="Download results as csv",
-            data=open(csv_file_path_file_path, 'rb').read(),
+            label="Download results as CSV",
+            data=open(csv_file_path, 'r').read(),
             file_name='results.csv'
         )
 
