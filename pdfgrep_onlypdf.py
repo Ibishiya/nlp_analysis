fa0b1e2f6304c62969552d0f6c7de85923730458
LadyJ <91752544+Ibishiya@users.noreply.github.com>
2024-07-24 03:21:18 +0000

    twenty

--- pdfgrep_onlypdf.py
+++ pdfgrep_onlypdf.py
@@ -53,15 +53,15 @@ if uploaded_files:
     if st.button('Save Results'):
         st.text("Generating results files...")
         # Save as text file
-        all_results_txt = "\n".join([f"Results for {result['file_path']}:\n{result['output']}'" for result in results])
+        all_results_txt = "\n".join([f"Results for {result['file_path']}:\n{result['output']}" for result in results])
         txt_file_path = '/tmp/results.txt'
         with open(txt_file_path, 'w') as f:
             f.write(all_results_txt)
         
-        # Save as Excel file
+        # Save as CSV file
         df = pd.DataFrame(results)
-        excel_file_path = '/tmp/results.xlsx'
-        df.to_excel(excel_file_path, index=False)
+        csv_file_path = '/tmp/results.csv'
+        df.to_csv(csv_file_path, index=False)
 
         # Provide download buttons
         st.download_button(
@@ -71,9 +71,20 @@ if uploaded_files:
         )
         
         st.download_button(
-            label="Download results as Excel",
-            data=open(excel_file_path, 'rb').read(),
-            file_name='results.xlsx'
+            label="Download results as CSV",
+            data=open(csv_file_path, 'r').read(),
+            file_name='results.csv'
         )
 
     if results and st.button('Generate EDA Report'):
+        st.text("Generating EDA report...")
+        df = pd.DataFrame(results)
+        eda_report = sv.analyze(df)
+        eda_report_file = '/tmp/eda_report.html'
+        eda_report.show_html(eda_report_file, open_browser=False)
+
+        st.download_button(
+            label="Download Report",
+            data=open(eda_report_file, 'r').read(),
+            file_name='eda_report.html'
+        )
